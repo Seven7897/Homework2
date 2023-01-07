dc45f9b80093f1087a335c831bb6a44453d6968a
Anatoly <107132732+Seven7897@users.noreply.github.com>
2023-01-07 21:32:43 +0300

    home2

---
+++ ProgramTask1.java
@@ -0,0 +1,25 @@
+package Homework2;
+
+import java.util.Scanner;
+
+public class ProgramTask1 {
+    public static void main(String[] args) {
+        System.out.println(GiveMeFloat());
+    }
+
+    public static float GiveMeFloat() {
+        Scanner in = new Scanner(System.in);
+        float number;
+
+        System.out.println("Введите число: ");
+        while (true) {
+            try {
+                number = Float.valueOf(in.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Введите число типа float!");
+            }
+        }
+        return number;
+    }
+}
